32bcb39a16c60b373785689d1b41f441be25d099
gorbov <usachborodach@gmail.com>
2026-07-29 00:30:02 +0300

    renamed:    utilities/mongo_backup/mongo_backup.py -> utilities/periodic_tasks/mongo_backup.py

--- utilities/periodic_tasks/daily.py
+++ utilities/periodic_tasks/daily.py
@@ -1,9 +1,3 @@
-import sys
-import os
-base_path = os.path.dirname(__file__)
-
-mongo_backup_path = f'{base_path}/../mongo_backup'
-sys.path.append(mongo_backup_path)
 from mongo_backup import mongo_backup
 
 def main():
