80364b8e9c424dd29e02ec0002da1642e0219242
gorbov <usachborodach@gmail.com>
2026-08-09 12:17:42 +0300

    renamed:    commit.sh -> sync.sh

--- sync.sh
+++ sync.sh
@@ -2,6 +2,7 @@
 git add .
 git commit
 git push
+
 ssh myfirstvps "cd myfirstvps && git pull"
 
 ssh myfirstvps "systemctl restart timetracker"
---
+++ sync_with_restart.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+git add .
+git commit
+git push
+
+ssh myfirstvps "cd myfirstvps && git pull"
+
+ssh myfirstvps "systemctl restart timetracker"
+ssh myfirstvps "systemctl status timetracker"
+
+ssh myfirstvps "systemctl restart quote_gun"
+ssh myfirstvps "systemctl status quote_gun"
