e26cb1add75e9633b695700f4b2fb89bd7cef0de
gorbov <usachborodach@gmail.com>
2026-08-09 11:59:48 +0300

    modified:   commit.sh

--- commit.sh
+++ commit.sh
@@ -2,7 +2,9 @@
 git add .
 git commit
 git push
-ssh myfirstvps "cd myfirstvps && git pull"
-
-systemctl restart timetraker
-systemctl restart quote_gun
+ssh myfirstvps "\
+    cd myfirstvps && \
+    git pull \
+    systemctl restart timetraker \
+    systemctl restart quote_gun \
+    "
