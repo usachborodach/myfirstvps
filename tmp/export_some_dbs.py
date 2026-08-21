bd7348562654808b6ffc96e38862f82c15ec1d67
gorbov <usachborodach@gmail.com>
2026-08-21 22:47:13 +0300

    modified:   tmp/export_some_dbs.py

--- tmp/export_some_dbs.py
+++ tmp/export_some_dbs.py
@@ -1,9 +1,23 @@
+# from myfirstvps.utilities.api_client.crud import get_all_docs
+
+# dbs = ['quote_gun', 'tracker']
+
+# for db in dbs:
+#     get_all_docs(db)
+
+
 from pymongo import MongoClient
 
 ADDRESS = 'localhost'
-PORT = 27017
+port = 29017
 
-client = MongoClient(ADDRESS, PORT)
-databases = client.list_database_names()
-for db_name in databases:
-    print(db_name)
+client = MongoClient(ADDRESS, port)
+
+dbs = ['quote_gun', 'tracker']
+
+for db_name in dbs:
+    db = client[db_name]               # получаем объект базы данных
+    collections = db.list_collection_names()   # список имён коллекций
+    print(f"База '{db_name}':")
+    for col in collections:
+        print(f"  - {col}")
