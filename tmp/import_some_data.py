335b9ae2da480503af47e353efaedd8f2dd15856
gorbov <usachborodach@gmail.com>
2026-08-21 23:34:35 +0300

    modified:   tmp/import_some_data.py

--- tmp/import_some_data.py
+++ tmp/import_some_data.py
@@ -1,10 +1,17 @@
 import os
 import yaml
+from pymongo import MongoClient
 
 base_path = os.path.dirname(__file__)
 os.chdir(base_path)
 
+client = MongoClient()
+
 for filename in os.listdir('exported_data'):
     file = open(f'exported_data/{filename}')
-    data = yaml.safe_load(file)
-    print(data)
+    docs = yaml.safe_load(file)
+    splitted_filename = filename.split('.')
+    db_name, collection_name = splitted_filename[0], splitted_filename[1]
+    db = client[db_name]
+    collection = db[collection_name]
+    collection.insert_many(docs)
