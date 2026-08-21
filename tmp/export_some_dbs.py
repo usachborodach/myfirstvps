08cb4adf5bd99a26357e981a911e2674087a4759
gorbov <usachborodach@gmail.com>
2026-08-21 22:41:07 +0300

    new file:   tmp/export_some_dbs.py

---
+++ tmp/export_some_dbs.py
@@ -0,0 +1,9 @@
+from pymongo import MongoClient
+
+ADDRESS = 'localhost'
+PORT = 29017
+
+client = MongoClient(ADDRESS, PORT)
+databases = client.list_database_names()
+for db_name in databases:
+    print(db_name)
