79d7a4308091d78171bd7c4e9bd2580fe2c63106
gorbov <usachborodach@gmail.com>
2026-08-22 12:28:38 +0300

    modified:   utilities/reminder/send_to_tg_and_wekan.py

--- utilities/reminder/send_to_tg_and_wekan.py
+++ utilities/reminder/send_to_tg_and_wekan.py
@@ -4,15 +4,16 @@ import csv
 import logger
 
 base_path = os.path.dirname(__file__)
-wekan_path = f'{base_path}/../wekan'
+wekan_path = f'{base_path}/../..'
 sys.path.append(wekan_path)
-from common import post_card
+import wekan
 
 def send_to_tg_and_wekan(index):
     reminders = csv.load()
     reminder = reminders[index]
     text_with_newlines = reminder['text'].replace('\\n', '\n')
-    post_card(f"⏰ {text_with_newlines}", reminder['board'])
+    token = wekan.common.get_token()
+    wekan.common.post_card(f"⏰ {text_with_newlines}", reminder['board'], token)
     reminders[index]['sended'] = '1'
     logger.log_debug(f"send_to_wekan ({reminder['text']}) function doned successfully")
     csv.dump(reminders)
