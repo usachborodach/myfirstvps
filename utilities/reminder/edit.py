f432308a4528285461a5f379823bbc5ea2d0fe19
gorbov <usachborodach@gmail.com>
2026-08-22 13:57:07 +0300

    modified:   utilities/reminder/edit.py

--- utilities/reminder/edit.py
+++ utilities/reminder/edit.py
@@ -1,8 +1,23 @@
+import subprocess
+
+SOURCE_PATH = '/root/myfirstvps/utilities/reminder/reminders.csv'
+LOCAL_PATH = '/tmp/reminders.csv'
+
 def main():
-    upload_csv()
+    download_csv()
+    open_with_nano()
+
+def download_csv():
+    command = ['scp', f'myfirstvps:{SOURCE_PATH}', LOCAL_PATH]
+    subprocess.run(command)
+
+def open_with_nano():
+    command = ['nano', LOCAL_PATH]
+    subprocess.run(command)
 
 def upload_csv():
-    command = ['scp', 'myfirstvps', '/root/myfirstvps/utilities/reminder/reminders.csv']
+    command = ['scp', LOCAL_PATH, f'myfirstvps:{SOURCE_PATH}']
+    subprocess.run(command)
 
 if __name__ == '__main__':
     main()
