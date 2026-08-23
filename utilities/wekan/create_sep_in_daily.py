2c4be1b78a9082460f1a70cab718b97524c7ca90
gorbov <usachborodach@gmail.com>
2026-08-23 15:30:20 +0300

    modified:   utilities/wekan/create_sep_in_daily.py

--- utilities/wekan/create_sep_in_daily.py
+++ utilities/wekan/create_sep_in_daily.py
@@ -1,12 +1,14 @@
 import common
 from datetime import datetime
 
-# поправить везде post_card
-
 def main():
     today_str = get_today_str()
     token = common.get_token()
-    # common.post_card(today_str, 'work', 'Дейлик', token)
+
+    common.post_card(today_str, 'work', 'Новые', token)
+    exit()
+
+
     client, db = common.connect_to_mongo()
     list_id = common.get_list_id('work', 'Дейлик')
     min_sort_val = get_min_sort_val(db, list_id)
