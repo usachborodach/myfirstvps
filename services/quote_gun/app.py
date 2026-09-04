3f6cd3d11b78b992eab6a4d2c65e045d949f23aa
gorbov <usachborodach@gmail.com>
2026-09-04 13:38:15 +0300

    modified:   services/quote_gun/app.py

--- services/quote_gun/app.py
+++ services/quote_gun/app.py
@@ -1,9 +1,7 @@
 from flask import Flask, render_template, send_from_directory
 from pymongo import MongoClient
-import argparse
 
 app = Flask(__name__)
-
 client = MongoClient()
 db = client['quote_gun']
 quotes_collection = db['quotes']
@@ -18,7 +16,7 @@ def all():
 def main():
     categories = quotes_collection.distinct('category')
     categories.sort()
-    exclude = ['svetlana_anatolyevna']
+    exclude = ['svetlana_anatolyevna', 'shtomnepokushac']
     for i in exclude:
         categories.remove(i)
     return render_template('main.html', categories=categories)
