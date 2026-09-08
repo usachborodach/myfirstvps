4868b5e6aebd0aca34ecb73fb5555e73a53d46d4
gorbov <usachborodach@gmail.com>
2026-09-08 15:44:17 +0300

    renamed:    utilities/wekan/remove_archived_objects.py -> utilities/wekan/remove_objects.py

--- utilities/wekan/remove_objects.py
+++ utilities/wekan/remove_objects.py
@@ -1,6 +1,6 @@
 from common import connect_to_mongo
 
-def main():
+def archived():
     client, db = connect_to_mongo()
     col_names = ['cards', 'lists']
     for col_name in col_names:
@@ -9,5 +9,13 @@ def main():
         collection.delete_many(query)
     client.close()
 
+def activities():
+    client, db = connect_to_mongo()
+    collection = db['activities']
+    query = {}
+    collection.delete_many(query)
+    client.close()
+
 if __name__ == '__main__':
-    main()
+    archived()
+    activities()
