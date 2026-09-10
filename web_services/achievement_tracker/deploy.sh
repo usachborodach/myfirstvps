c5001d8a35fd4db3a7cc3c96b70a980c916370e4
gorbov <usachborodach@gmail.com>
2026-09-10 21:58:24 +0300

    начал революцию в папках

--- web_services/achievement_tracker/deploy.sh
+++ web_services/achievement_tracker/deploy.sh
@@ -1,14 +1,8 @@
 # SECRET_KEY
-```
 python3 -c "import secrets; print(secrets.token_hex(32))"
-```
 
 # PASSWORD_HASH
-```
 python -c "from werkzeug.security import generate_password_hash; print(generate_password_hash('мой_пароль'))"
-```
 
 # LOG DIR
-```
 mkdir -p /var/log/achievement_tracker
-```
