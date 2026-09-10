bea6c8c6baadb6b4c9ea36bf64e43279e0563f7c
gorbov <usachborodach@gmail.com>
2026-09-10 22:09:23 +0300

    modified:   web_services/achievement_tracker/deploy.sh

--- web_services/achievement_tracker/deploy.sh
+++ web_services/achievement_tracker/deploy.sh
@@ -1,8 +1,21 @@
+# install dependencies
+pip3 install -r requirements.txt
+
 # SECRET_KEY
 python3 -c "import secrets; print(secrets.token_hex(32))"
 
 # PASSWORD_HASH
 python -c "from werkzeug.security import generate_password_hash; print(generate_password_hash('мой_пароль'))"
 
-# LOG DIR
+# make log dir
 mkdir -p /var/log/achievement_tracker
+
+# systemd service file symlink
+ln -s /root/myfirstvps/web_services/systemd.conf /etc/systemd/system/achievement_tracker.service
+systemctl daemon-reload
+systemctl enable --now achievement_tracker.servic
+
+# nginx config file symlink
+ln -s /root/myfirstvps/web_services/nginx.conf /etc/nginx/sites-enabled/achievement_tracker
+nginx -t
+systemctl restart nginx
