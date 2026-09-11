49cc6af3e665f689cfd4a4c7e74bab0f69ec97b9
gorbov <usachborodach@gmail.com>
2026-09-11 12:28:37 +0300

    new file:   web_services/achievement_tracker/clear_logs_and_restart.sh

--- web_services/achievement_tracker/clear_log.sh
+++
@@ -1,2 +0,0 @@
-rm /var/log/nginx/achievement_tracker*
-rm /var/log/achievement_tracker/*
---
+++ web_services/achievement_tracker/clear_logs_and_restart.sh
@@ -0,0 +1,4 @@
+rm /var/log/nginx/achievement_tracker*
+rm /var/log/achievement_tracker/*
+systemctl restart nginx
+systemctl restart achievement_tracker
